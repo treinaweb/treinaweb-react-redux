2cfee971434677e0b6d451909f79d995e085d3db
Akira Hanashiro <felipehanashiro@hotmail.com>
2019-01-23 10:45:28 -0200

    aula-04-01

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from './data/reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
     <Provider store={store} >
