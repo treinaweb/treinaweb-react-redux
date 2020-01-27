314fd58986a02f9652b034023edb89911036e625
hanashiro <felipeaah@gmail.com>
2020-01-27 10:21:25 -0300

    aula-03-08

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import ToDoList from './views/components/ToDoList';
 import NewToDoItem from './views/components/NewToDoItem';
 
 import * as TodoActions from './data/actions/TodoActions';
-import { connect } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
+
+function MeusHooks(){
+  const dispatch = useDispatch();
+  const list = useSelector((state) => {return state.TodoReducer});
+
+  console.log(list)
+  return (<div onClick={() => dispatch(TodoActions.clear())} >OI</div>);
+}
 
 class App extends Component {
   render() {
@@ -13,6 +21,7 @@ class App extends Component {
       { dispatch } = props;
     return (
       <div className="App">
+        <MeusHooks />
         <NewToDoItem onAdd={(description) => { dispatch(TodoActions.create(description)) } } />
         <hr />
         <button className="tw-btn" onClick={() => { dispatch(TodoActions.clear())} } >Limpar</button>
